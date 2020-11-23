885b037901193c87dfc4559253872139eaf6d6c2
Martin Petoffer <martin.petoffer@uptime.eu>
2020-11-23 17:03:08 +0200

    Init

---
+++ EFCore5TPTBug/Migrations/20201123150221_Init.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore5TPTBug.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "referral",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("referral_pkey", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_referral",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("referral_pkey", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_user_referral_referral_Id",
+                        column: x => x.Id,
+                        principalTable: "referral",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_referral");
+
+            migrationBuilder.DropTable(
+                name: "referral");
+        }
+    }
+}
---
+++ EFCore5TPTBug/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace EFCore5TPTBug
+{
+    public class Referral
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class UserReferral : Referral
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class ReferralEntityConfiguration : IEntityTypeConfiguration<Referral>
+    {
+        public void Configure(EntityTypeBuilder<Referral> builder)
+        {
+            // This causes all derived classes to use the same key name and PostgreSQL does not like that
+            // PostgreSQL error 42P07: relation "referral_pkey" already exists
+            builder.HasKey(p => p.Id).HasName("referral_pkey");
+
+            builder.ToTable("referral");
+        }
+    }
+
+    public class UserReferralEntityConfiguration : IEntityTypeConfiguration<UserReferral>
+    {
+        public void Configure(EntityTypeBuilder<UserReferral> builder)
+        {
+            // Trying to change the key name for a derived type will get you this exception
+            // System.InvalidOperationException: A key cannot be configured on 'UserReferral' because it is a derived type. The key must be configured on the root type 'Referral'.
+
+            //builder.HasKey(p => p.Id).HasName("user_referral_pkey");
+
+            builder.ToTable("user_referral");
+        }
+    }
+
+
+    public class TestDbContext : DbContext
+    {
+        public DbSet<Referral> Referrals { get; set; }
+        public DbSet<UserReferral> UserReferrals { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=efcore5tptbug;Username=postgres;Password=;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TestDbContext).Assembly);
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
